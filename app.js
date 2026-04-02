01d89a6f050720cafa8ccd8af80fc103c3789a96
Prajwalsp83 <97027168+Prajwalsp83@users.noreply.github.com>
2026-04-02 14:07:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello mfs 🚀 Version 2');
+  res.send('Hello psp 🚀 Version 2');
 });
 
 app.listen(3000, () => {
