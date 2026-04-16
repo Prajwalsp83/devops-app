de270c418ed187e1aeb333b5d2e23516041ccf5b
Prajwalsp83 <97027168+Prajwalsp83@users.noreply.github.com>
2026-04-16 23:22:37 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "app" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t3.small"
+  key_name      = "your-keypair-name"
+
+  security_groups = ["default"]
+
+  tags = {
+    Name = "devops-app"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.app.public_ip
+}
